0768a143a62f5be11aef91726929bb54654ac5b7
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2022-12-05 16:17:18 +0100

    feat(crearArchivos.py): añadido modulo que crear el archivo mediante los parámetros recibidos.

---
+++ src/capaPresentacion/crearArchivos.py
@@ -0,0 +1,14 @@
+import os
+# Este modulo se va a encargar de crear el archivo demandado en la ruta especificada.
+
+# Pasandole estos parámetros, conseguimos que cree un archivo con la información especificada.
+
+def crearArchivo(contenido, ruta, nombre, tipo):
+
+    ruta = os.path.relpath(ruta) # Definimos la ruta relativa 
+    try:
+        with open(f"{ruta}\\{nombre}.{tipo}","w", encoding="utf-8") as archivo: # Le asignamos los parametros detallados, creamos el archivo
+            archivo.write(contenido) # Escribimos el contenido en el archivo
+            print("El archivo 'index.html' creado correctamente.")
+    except FileNotFoundError: # Captamos una excepción en caso de que no encuentre el directorio.
+        print("El directorio no existe, ejecuta correctamente el programa y vuelve a intentarlo.")
