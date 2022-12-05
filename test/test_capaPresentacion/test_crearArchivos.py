05200341cf477fd45626154eb3c36df72c473a68
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2022-12-05 22:00:26 +0100

    test(crearArchivos.py): añadidos casos test para el modulo crearArchivos

---
+++ test/test_capaPresentacion/test_crearArchivos.py
@@ -0,0 +1,19 @@
+from src.capaPresentacion.crearArchivos import crearArchivo
+import pytest 
+import os
+
+@pytest.mark.test_crearArchivo
+def test_crearArchivo():
+
+    crearArchivo("hola", "..\\", "archivo", "txt")
+    ruta = os.path.relpath("..\\archivo.txt")
+    assert os.path.isfile(ruta) == True
+
+
+@pytest.mark.test_noExisteDirectorio
+def test_noExisteDirectorio():
+
+
+    crearArchivo("hola", "..\\docs", "archivo", "txt")
+    ruta = os.path.relpath(".\\docs\\prueba.txt")
+    assert os.path.isfile(ruta) == False
