02a79eda8c181c0502de3f403b05f3ee645b9359
Gema Marquinez <gmarquinezquiroz@cifpfbmoll.eu>
2022-12-06 21:33:51 +0100

    refactor (crearMarcasCss.py): adaptando el codigo a la estructura de carpetas

--- src/capaPresentacion/crearCss/crearSecundarioCss.py
+++ src/capaPresentacion/crearCss/crearSecundarioCss.py
@@ -7,14 +7,8 @@ def crearBicisCss():
 def crearBiciSolitariaCss():
     crearArchivo(biciSolitariaStylesCss(), ".\\docs\\cssStyles", "bicisolitaria", "css")
 
-def crearMarcasCss():
-    crearArchivo(marcasStylesCss(), ".\\docs\\cssStyles", "marcas", "css")
 
-def crearRentalsCss():
-    crearArchivo(rentalsStylesCss(), ".\\docs\\cssStyles", "rentals", "css")
 
-def crearTiposCss():
-    crearArchivo(tiposStylesCss(), ".\\docs\\cssStyles", "tipos", "css")
 
 def crearBicisFiltroCss():
     crearArchivo(bicisfiltroStylesCss(), ".\\docs\\cssStyles", "bicisfiltro", "css")
