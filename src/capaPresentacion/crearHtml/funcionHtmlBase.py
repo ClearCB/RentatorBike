01b834df354cac1f7cbdfe8be34a3342af0cacdd
ClearCB <abelcasasccb@gmail.com>
2022-12-07 20:03:00 +0100

    update(remote:pagina): actualizar informacion de la base de datos en gitHub Pages.

--- src/capaPresentacion/crearHtml/funcionHtmlBase.py
+++ src/capaPresentacion/crearHtml/funcionHtmlBase.py
@@ -21,7 +21,7 @@ def crearHtmlHead(titulo, descripcion, keywords, cssPath, cssLink):
         <meta name="generator" content="Visual Studio Code">
         <meta name="keywords" content="{keywords}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" type="image/jpg" href="http://imgfz.com/i/MtOFPGk.jpeg">
+        <link rel="icon" type="image/x-icon" href="http://drive.google.com/uc?export=view&id=1Av6cR8J-UoaPBDbQbuRp1HmjdYnXxutB">
         <link rel="stylesheet" type="text/css" href="{cssPath}footer.css">
         <link rel="stylesheet" type="text/css" href="{cssPath}header.css">
         <link rel="stylesheet" type="text/css" href="{cssPath}base.css">
