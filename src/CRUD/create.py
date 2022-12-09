d4ae4c5b00e889fc2f131c470aca0bb0d8e8aa85
ClearCB <abelcasasccb@gmail.com>
2022-12-09 18:01:07 +0100

    feat(CRUD): añadido directorio que se va encargar de la funcionalidad CRUD, interacción con la base de datos.

---
+++ src/CRUD/create.py
@@ -0,0 +1,3 @@
+# En este modulo se van a definir todas las funcionalidades necesárias para ejecutar una consulta de
+# creación de documentos en mongoDB, posteriormente, la uniremos a un programa principal que ejecutara
+# una interfaz gráfica que nos será útil para guiar al usuario a ejecutar dichos comandos. 
---
+++ src/CRUD/crud.py
@@ -0,0 +1,2 @@
+# Este módulo se va a encargar de reunir las cuatro principales funcionalidades del crud y
+# y de generar una interfaz gráfica con la cual guiar al usuario en sus consultas.
