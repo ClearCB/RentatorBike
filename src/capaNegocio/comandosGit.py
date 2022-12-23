ff239b6749372d0acd27dfc7f37eb47b79586679
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2022-12-23 09:56:45 +0100

    update(remote:pagina): actualizar informacion de la base de datos en gitHub Pages.

--- src/capaNegocio/comandosGit.py
+++ src/capaNegocio/comandosGit.py
@@ -4,39 +4,48 @@ import subprocess
 # visualizar los cambios efectuados en local, en la rama "página" de github
 # que será la que se conecte a github pages.
 
-# Definimos una función que ejecuta el comando gitpull
-def comandoGit(comando):
-
-    try:
-        comand = subprocess.run(comando, shell=True)
-        if comand.returncode != 0 and comand.returncode != 1:
-            raise Exception
-        else:
-            print(f"El comando {comando} se ha ejecutado correctamente")
-            return True
-    except:
-        print(f"El comando {comando} no se ha podido ejecutar correctamente, revíselo e inténtelo de nuevo.")
-        return False
-
 # Definimos una función que ejecuta el comando gitad
 def gitAd():
 
-    comandoGit("git add .")
+    try: # Comprobamos que el comando se puede ejecutar sin problema
+        comandoAdd = "git add ."
+        subprocess.run(comandoAdd)
+        print("Comando git add ejecutado correctamente.")
+
+    except subprocess.SubprocessError: # Capturamos un error en el caso de que no se pueda ejecutar correctamente
+        print("El comando git add no se ha podido ejecutar correctamente, revíselo e inténtelo de nuevo.")
 
 # Definimos una función que ejecuta el comando gitcommit 
 def gitCommit():
 
-    comandoGit("git commit -m 'Actualizar'")
+    try: # Comprobamos que el comando se puede ejecutar sin problema
+        comandoCommit = f'git commit -m "update(remote:pagina): actualizar informacion de la base de datos en gitHub Pages."'
+        subprocess.run(comandoCommit)
+        print("Comando git commit ejecutado correctamente.")
+
+    except subprocess.SubprocessError: # Capturamos un error en el caso de que no se pueda ejecutar correctamente
+        print("El comando git commit no se ha podido ejecutar correctamente, revíselo e inténtelo de nuevo.")
 
 def gitPull():
 
-    comandoGit("git pull origin main")
+    try: # Comprobamos que el comando se puede ejecutar sin problema
+        comandoDelete = f"git pull origin main"
+        subprocess.run(comandoDelete)
+        print("Comando git delete ejecutado correctamente.")
+
+    except subprocess.SubprocessError: # Capturamos un error en el caso de que no se pueda ejecutar correctamente
+        print("El comando git delete no se ha podido ejecutar correctamente, revíselo e inténtelo de nuevo.")
 
 # Definimos una función que ejecuta el comando gitpush
 def gitPush():
 
-    comandoGit("git push origin main")
-    print("Los cambios han sido actualizado para visitar la pagina web visite el siguiente link:'LINK'")
+    try: # Comprobamos que el comando se puede ejecutar sin problema
+        comandoPush = f"git push origin main"
+        subprocess.run(comandoPush)
+        print("Comando git push ejecutado correctamente.")
+
+    except subprocess.SubprocessError: # Capturamos un error en el caso de que no se pueda ejecutar correctamente
+        print("El comando git push no se ha podido ejecutar correctamente, revíselo e inténtelo de nuevo.")
 
 # Definimos una función que ejecuta los comandos gitad y git commit para guardar los cambios
 def guardarCambios():
@@ -54,3 +63,5 @@ def actualizarPages():
 
     guardarCambios()
     actualizarGitHubPagina()
+
+actualizarPages()
